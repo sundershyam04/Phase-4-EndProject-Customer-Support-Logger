12e2f0ec9da423e2f10cd8351564e67466036913
sundershyam04 <60753448+sundershyam04@users.noreply.github.com>
2022-11-04 12:25:18 +0530

    DAL and DALTest

---
+++ DAL/CSLoperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CSLoperations
+    {
+
+        csldbEntities context;
+
+        public CSLoperations()
+        {
+            context = new csldbEntities();
+        }
+
+        public bool ValidateUser(int u, string email,string pw)
+        {
+
+            var found = context.UserInfoes.ToList().Find(w => ((w.UserId == u) && (w.Email == email) && (w.Password == pw)));
+
+            return found != null;
+        }
+
+        public bool Registerlog(CustLogInfo c)
+        {
+            try
+            {
+                var done = context.CustLogInfoes.Add(c);
+                context.SaveChanges();
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ DALTest/CSLTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace DALTest
+{
+    [TestFixture]
+    public class CSLTests
+    {
+        CSLoperations operations;
+        public CSLTests()
+        {
+           operations = new CSLoperations();
+        }
+
+        //UserInfoTest
+        //[Test]
+
+        [TestCase(1,"ravi@gmail.com","Ravi@123",ExpectedResult =true)]
+        public bool ValidateUser(int user, string email,string pw)
+        {
+           
+            var found = operations.ValidateUser(user,email, pw);
+
+            return found;
+
+        }
+
+        //custloginfo
+
+        [Test]
+        public bool GetAllLoginInfoTest()
+        {
+            csldbEntities context = new csldbEntities();
+            var got = context.CustLogInfoes.ToList();
+            return got != null;
+        }
+
+        [Test]
+        [TestCase("Ravi", ExpectedResult = true)]
+        [TestCase("Ram", ExpectedResult = false)]
+        public bool SaveCustLogInfoTest(string s)
+        {
+            csldbEntities context = new csldbEntities();
+            var found = context.CustLogInfoes.ToList().Find(c => c.CustName == s);
+
+            return found != null;
+
+        }
+
+
+    }
+}
+
